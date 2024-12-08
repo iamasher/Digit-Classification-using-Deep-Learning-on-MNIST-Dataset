31cc170d6c9fe1ca03d4f6b5fc6ec5b409c4dae0
Md Asher <97349369+iamasher@users.noreply.github.com>
2024-12-08 18:59:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #This file will contain the python code for this project
+import tensorflow as tf                           #importing required libraries
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+
 #Do-Not-Modify-This-File
 #To be updated
